c02b92bf7de3ab10fc26365b1c707dc928102882
T R <tylor.reeves@ibm.com>
2017-03-28 16:24:49 -0500

    Added bootstrap and basic ui/TS function prototypes

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { URLSearchParams } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http'
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+	constructor (private http: Http) {}
+
+	supplierSearch(company, product) {
+		let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+		let options = new RequestOptions({ headers: headers });
+		let urlSearchParams = new URLSearchParams();
+		urlSearchParams.append('company', company);
+		urlSearchParams.append('product', product);
+		let body = urlSearchParams.toString();
+
+		return this.http.post('/api/discovery/company/product', body, options)
+			.map((res: Response) => res.json())
+			.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+	}
+
+	supplierAll() {
+		return this.http.get('/api/discovery/all')
+			.map((res: Response) => console.log());
+
+	}
+	
 }
