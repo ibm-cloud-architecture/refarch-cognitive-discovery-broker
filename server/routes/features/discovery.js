6ffb4bfb57d5e1844852dff01a626acce9696dec
Tylor Reeves <reeves.tylor@gmail.com>
2017-04-12 16:54:23 -0500

    Added query-builder, fixed env-templ and Discovery instance creation

--- server/routes/features/discovery.js
+++ server/routes/features/discovery.js
@@ -1,9 +1,11 @@
 const cfenv = require('cfenv');
-const watson = require('watson-developer-cloud');
 const extend = require('extend');
+const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 const vcapServices = require('vcap_services');
 const express = require('express');
 const async = require('async');
+
+const queryBuilder = require('./query-builder');
 
 var app = express();
 
@@ -13,19 +15,20 @@ const router = express.Router();
 var appEnv = cfenv.getAppEnv();
 var config = require('../env.json');
 
-var discConfig = extend(config.discovery, vcapServices.getCredentials('discovery'));
-var discovery = watson.discovery({
+const discConfig = extend(config.discovery, vcapServices.getCredentials('discovery'));
+const discovery = new DiscoveryV1({
   username: discConfig.username,
   password: discConfig.password,
-  version: discConfig.version,
-  version_date: discConfig.version_date
+  version_date: discConfig.version_date,
+  path: {
+    environment_id: discConfig.environment_id,
+    collection_id: discConfig.collection_id
+  },
+  qs: { aggregation: `[${queryBuilder.aggregations.join(',')}]` },
 });
 
 router.get('/all', function(req, res) {
-	discovery.query({
-    environment_id: discConfig.environment_id,
-    collection_id: discConfig.collection_id
-  }, function(err, response) {
+  discovery.query({}, function(err, response) {
         if (err) {
           console.error(err);
         } else {
@@ -35,55 +38,32 @@ router.get('/all', function(req, res) {
 });
 
 router.post('/company/product', function(req, res) {
-	var productQuery;
-	var companyQuery;
-  console.log(req.body);
-	if (req.body.product) productQuery = req.body.product + ",language:english";
-	else  productQuery = "language:english";
-	if (req.body.company) companyQuery = "blekko.urlrank>1,blekko.chrondate>1482901200,enrichedTitle.entities.text:" + req.body.company;
-	else companyQuery = "blekko.urlrank>1,blekko.chrondate>1482901200"
-
-	discovery.query({
-    environment_id: discConfig.environment_id,
-    collection_id: discConfig.collection_id,
-    count: "5",
-    query: productQuery,
-    filter: companyQuery,
-    return: "title,docSentiment,enrichedTitle.text,url,host,enrichedTitle.entities.text",
-    aggregation: [
-    "nested(enrichedTitle.entities).filter(enrichedTitle.entities.type:Company).term(enrichedTitle.entities.text)",
-    "nested(enrichedTitle.entities).filter(enrichedTitle.entities.type:Person).term(enrichedTitle.entities.text)",
-    "term(enrichedTitle.concepts.text)",
-    "term(blekko.basedomain).term(docSentiment.type)",
-    "term(docSentiment.type)",
-    "min(docSentiment.score)",
-    "max(docSentiment.score)",
-    "filter(enrichedTitle.entities.type::Company).term(enrichedTitle.entities.text).timeslice(blekko.chrondate,1day).term(docSentiment.type)"
-  ]
-  }, function(err, response) {
-        if (err) {
-          console.error("Error "+err);
-        } else {
-        	var returnJSON = [];
-          console.log("Discovery response "+response.results)
-        	async.forEach(response.results, function(result, callback) {
-        		returnEntities = [];
-          		async.forEach(result.enrichedTitle.entities, function(entity, callback) {
-          			returnEntities.push(entity.text);
-          			callback();
-          		}, function(err) {
-                // data mapping can be done here
-          			returnJSON.push({score: result.score,
-                  url: result.url,
-                  title: result.title,
-                  sentiment:result.docSentiment.score, entities: returnEntities});
-          			callback();
-          		});
-          	}, function(err) {
-          		res.json(returnJSON);
-        	});
-        }
-   });
+  const params = queryBuilder.build(req.body);
+  discovery.query(params, function(err, response) {
+    if (err) {
+      console.error("Error "+err);
+    } else {
+      var returnJSON = [];
+      console.log("Discovery response "+response.results)
+      async.forEach(response.results, function(result, callback) {
+        returnEntities = [];
+        	async.forEach(result.enrichedTitle.entities, function(entity, callback) {
+        		returnEntities.push(entity.text);
+      			callback();
+      		}, function(err) {
+            // data mapping can be done here
+      			returnJSON.push({score: result.score,
+              url: result.url,
+              title: result.title,
+              sentiment:result.docSentiment.score, entities: returnEntities
+            });
+            callback();
+          });
+        }, function(err) {
+          res.json(returnJSON);
+      });
+    }
+  });
 });
 
 module.exports = router;
---
+++ server/routes/features/query-builder.js
@@ -0,0 +1,46 @@
+// Aggregations used to build the different parts of the UI
+
+const entities = [
+  'nested(enrichedTitle.entities).filter(enrichedTitle.entities.type:Company).term(enrichedTitle.entities.text)',
+  'nested(enrichedTitle.entities).filter(enrichedTitle.entities.type:Person).term(enrichedTitle.entities.text)',
+  'term(enrichedTitle.concepts.text)',
+];
+
+const sentiments = [
+  'term(blekko.basedomain).term(docSentiment.type)',
+  'term(docSentiment.type)',
+  'min(docSentiment.score)',
+  'max(docSentiment.score)',
+];
+
+const mentions = [
+  // eslint-disable-next-line
+  'filter(enrichedTitle.entities.type::Company).term(enrichedTitle.entities.text).timeslice(blekko.chrondate,1day).term(docSentiment.type)'
+];
+
+module.exports = {
+  aggregations: [].concat(entities, sentiments, mentions),
+  entities,
+  sentiments,
+  mentions,
+  build(query, full) {
+    var productQuery;
+    var companyQuery;
+    //Creates date two weeks ago for query usage
+    var queryTime = new Date;
+    queryTime = Math.floor(queryTime.getTime()/1000) - 1209600;
+    if (query.product) productQuery = query.product + ",language:english";
+    else  productQuery = "language:english";
+    if (query.company) companyQuery = "blekko.urlrank>1,blekko.chrondate>" + queryTime + ",enrichedTitle.entities.text:" + query.company;
+    else companyQuery = "blekko.urlrank>1,blekko.chrondate>" + queryTime;
+
+    const params = {
+      count: 5,
+      return: 'title,docSentiment,enrichedTitle.text,url,host,enrichedTitle.entities.text',
+      query: productQuery,
+      aggregations: [].concat(entities, sentiments, mentions),
+      filter: companyQuery
+    };
+    return params;
+  },
+};
